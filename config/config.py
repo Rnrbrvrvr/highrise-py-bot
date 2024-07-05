a467bc045d8d278d652612b004098b82d799daec
Rnrbrvrvr <119675948+Rnrbrvrvr@users.noreply.github.com>
2024-07-05 08:38:20 +0300

    config.py

--- config/config.py
+++ config/config.py
@@ -44,5 +44,5 @@ class permissions:
 class authorization:
     # To obtain your token, visit https://highrise.game/ and log in. Then, go to the settings and create a new bot. Accept the terms and generate a token.
     # To obtain your room ID, go to the game and navigate to the top right corner where the player list is displayed. Click on "Share this room" and copy the ID.
-    room = 'change-me'
-    token = 'change-me'
+    room = '63f9a7d98e4ca57020c39341'
+    token = '20aae0e78bed893b5c1086a485faf36b92946621ded93cb83a0164bbfee0b00d'
